6d812f6f94d2e9592c77f578b58ded353f5dc3ab
smunozub <smunozub@emeal.nttdata.com>
2024-10-21 13:10:19 +0200

    button component created

--- src/app/components/main-container/main-container.component.ts
+++ src/app/components/main-container/main-container.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { StepCardComponent } from '../../shared/step-card/step-card.component';
 import { StepStatus } from '../../interfaces/interfaces';
+import { ButtonComponent } from '../../shared/button/button.component';
 
 @Component({
   selector: 'app-main-container',
   standalone: true,
-  imports: [StepCardComponent],
+  imports: [StepCardComponent, ButtonComponent],
   templateUrl: './main-container.component.html',
   styleUrl: './main-container.component.scss'
 })
 export class MainContainerComponent {
 
   stepStatus = StepStatus;
+
+  nextClicked(data: boolean) {
+    console.log(data);
+	}
 
 }
