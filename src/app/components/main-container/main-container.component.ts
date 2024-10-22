fd11b6d087c16ae112256fc7d6d2480434a851af
smunozub <smunozub@emeal.nttdata.com>
2024-10-22 12:29:13 +0200

    input component created

--- src/app/components/main-container/main-container.component.ts
+++ src/app/components/main-container/main-container.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
 import { StepCardComponent } from '../../shared/step-card/step-card.component';
 import { StepStatus } from '../../interfaces/interfaces';
+import { InputComponent } from '../../shared/input/input.component';
 
 @Component({
   selector: 'app-main-container',
   standalone: true,
-  imports: [StepCardComponent],
+  imports: [StepCardComponent, InputComponent],
   templateUrl: './main-container.component.html',
   styleUrl: './main-container.component.scss'
 })
+
 export class MainContainerComponent {
 
   stepStatus = StepStatus;
+  isRequired: boolean = true; // Estado inicial
+
+  receiveData(data: string) {
+    console.log(data);
+    
+  }
 
 }
---
+++ src/app/shared/input/input.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss'],
+  standalone: true,
+  imports: [MatCardModule, NgFor, NgIf, CommonModule, ReactiveFormsModule]
+})
+
+export class InputComponent implements OnInit {
+
+  @Output() dataEmitter: EventEmitter<string> = new EventEmitter<string>();
+
+  @Input() inputId: string = '';
+  @Input() inputData: string = '';
+  @Input() inputType: string = 'default'; // default or empty
+  @Input() inputRequired: boolean = false;
+  
+  @Input() labelText: string = '';
+
+  inputName: string = '';
+  
+  constructor( ) { }
+
+  ngOnInit(): void {
+    this.inputName = this.inputId;
+  }
+
+  sendData(event: any) {
+    this.dataEmitter.emit(event.target.value);
+  }
+}
