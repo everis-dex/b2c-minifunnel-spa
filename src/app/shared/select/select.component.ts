bb03cd4b8917b2f7fcea8a85909ab73e78d9c2e8
smunozub <smunozub@emeal.nttdata.com>
2024-10-23 13:06:57 +0200

    new styles added

--- src/app/shared/select/select.component.ts
+++ src/app/shared/select/select.component.ts
@@ -1,20 +1,19 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-select',
   standalone: true,
-  imports: [ MatSelectModule, NgFor, NgIf, CommonModule],
+  imports: [ MatSelectModule, NgFor, NgIf, CommonModule, ReactiveFormsModule],
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.scss']
 })
 
-export class SelectComponent implements OnInit {
+export class SelectComponent {
 
   @Input() selectId: string = '';
-  // @Input() selectType = 'primary'; // primary or secondary
-  // @Input() selectOptions: string[] = [];
   @Input() selectOptions: { value: string, label: string }[] = [];
 
   @Output() selectedOption: EventEmitter<string> = new EventEmitter<string>();
@@ -25,16 +24,9 @@ export class SelectComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
-  // sendData(event: any) {
-  //   this.selectedOption.emit(event.target.value);
-  // }
-
   selectOption(selectedValue: string) {
     this.optionSelected = selectedValue;
-    console.log(selectedValue); // Para verificar el valor seleccionado
-    this.selectedOption.emit(this.optionSelected); // Emitir el valor seleccionado
+    console.log(selectedValue);
+    this.selectedOption.emit(this.optionSelected);
   }
 }
