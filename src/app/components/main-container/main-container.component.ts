ccaeced018247d8f2fb50ac3f2268b7e0e12fa40
smunozub <smunozub@emeal.nttdata.com>
2024-10-23 17:15:23 +0200

    radiobutton component started

--- src/app/components/main-container/main-container.component.ts
+++ src/app/components/main-container/main-container.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { StepCardComponent } from '../../shared/step-card/step-card.component';
 import { StepStatus } from '../../interfaces/interfaces';
+import { RadiobuttonComponent } from '../../shared/radiobutton/radiobutton.component';
 
 @Component({
   selector: 'app-main-container',
   standalone: true,
-  imports: [StepCardComponent],
+  imports: [StepCardComponent, RadiobuttonComponent],
   templateUrl: './main-container.component.html',
   styleUrl: './main-container.component.scss'
 })
---
+++ src/app/shared/radiobutton/radiobutton.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { MatRadioButton } from '@angular/material/radio';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-radiobutton',
+  templateUrl: './radiobutton.component.html',
+  styleUrls: ['./radiobutton.component.scss'],
+  standalone: true,
+  imports: [NgFor, NgIf, CommonModule, MatRadioButton, MatIcon]
+})
+
+export class RadiobuttonComponent {
+
+  @Output() dataEmitter: EventEmitter<string> = new EventEmitter<string>();
+
+  @Input() radiobuttonId: string = '';  
+  @Input() radiobuttonText: string = '';
+
+  inputName: string = '';
+  
+  constructor( ) { }
+
+  // sendData(event: any) {
+  //   this.dataEmitter.emit(event.target.value);
+  // }
+}
